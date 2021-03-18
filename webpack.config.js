c75ec570a6be66ac3ba3ea25d9b6a315d7aa012f
Akshat <masterakshata@gmail.com>
2021-03-18 18:14:24 +0530

    set up basic project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+  })],
+};
