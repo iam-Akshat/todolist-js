b514aa4ef5763f44001b42f7514d17b8eb86d0f8
Akshat <masterakshata@gmail.com>
2021-03-18 19:41:17 +0530

    add id parameter to createTodo

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,16 +1,16 @@
 class Todo {
-  constructor (title, description, dueDate, priority) {
+  constructor(title, description, dueDate, priority, id) {
     this.title = title
     this.description = description
     this.dueDate = dueDate
     this.priority = priority
     this.completed = false
-    this.id = Date.now()
+    this.id = id
   }
 }
 
-const createTodo = (title, description, dueDate, priority) => {
-  let newTodo = new Todo(title, description, dueDate, priority);
+const createTodo = (title, description, dueDate, priority,id =  Date.now()) => {
+  let newTodo = new Todo(title, description, dueDate, priority,id);
   return newTodo;
 }
 
