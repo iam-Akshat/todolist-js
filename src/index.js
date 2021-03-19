8d9f0f97c94229e4300915b67fae2bfdefd4696d
Ahmad Chata <ahmadchata@gmail.com>
2021-03-19 16:13:31 +0100

    Display Projects

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ import LocalDb from "./models/localDb.js"
 const db = new LocalDb()
 const projects = db.populateData()
 const homeTodoList = projects[0].todoList
+const todo = document.querySelector('.todo');
 
 const addProjectButton = document.getElementById('add-project');
 
-
-
 addProjectButton.addEventListener('click', (e) => {
     addProjectFormHandler(e, db)
+    displayProject();
 })
 
+const displayProject = () => {
+  todo.innerHTML = ''
+  projects.forEach(pg => {
+    const proj = `<div>${pg.name}</div>`
+    todo.innerHTML += proj
+  })
+  }
 
-
-
+  displayProject();
