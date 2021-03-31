b4faf5d2af72139dbd03b76047d26f8000bbeefb
Akshat <masterakshata@gmail.com>
2021-03-31 14:26:05 +0530

    fix linter errors in project.test.js

--- tests/project.test.js
+++ tests/project.test.js
@@ -4,27 +4,27 @@ import createTodo from '../src/models/todo';
 describe('Project', () => {
   describe('createProject', () => {
     test('Should create a project', () => {
-    let myProject = createProject('Study');
-    expect(myProject).toEqual({name: "Study", todoList: []});
-  });
+      const myProject = createProject('Study');
+      expect(myProject).toEqual({ name: 'Study', todoList: [] });
+    });
   });
   describe('addTodo', () => {
     test('Should add todo to a project', () => {
-      let myProject = createProject('Rails');
-      let myTodo = createTodo("Rspec", "Testing with Rspec", "24-01-2021", "High", 1);
+      const myProject = createProject('Rails');
+      const myTodo = createTodo('Rspec', 'Testing with Rspec', '24-01-2021', 'High', 1);
       myProject.addTodo(myTodo);
-      expect(myProject).toEqual({name: "Rails", todoList: [myTodo]});
+      expect(myProject).toEqual({ name: 'Rails', todoList: [myTodo] });
     });
   });
   describe('deleteTodo', () => {
     test('Should delete todo from a project', () => {
-      let myProject = createProject('Rails');
-      let myTodo = createTodo("Rspec", "Testing with Rspec", "24-01-2021", "High", 1);
-      let anotherTodo = createTodo("Rspec", "Testing with Rspec", "24-01-2021", "High", 2)
+      const myProject = createProject('Rails');
+      const myTodo = createTodo('Rspec', 'Testing with Rspec', '24-01-2021', 'High', 1);
+      const anotherTodo = createTodo('Rspec', 'Testing with Rspec', '24-01-2021', 'High', 2);
       myProject.addTodo(myTodo);
       myProject.addTodo(anotherTodo);
       myProject.deleteTodo(2);
-      expect(myProject).toEqual({name: "Rails", todoList: [myTodo]});
+      expect(myProject).toEqual({ name: 'Rails', todoList: [myTodo] });
     });
   });
 });
