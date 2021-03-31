2a670696560731f82e1df1a66b6f3f308423f583
Ahmad Chata <ahmadchata@gmail.com>
2021-03-31 09:49:28 +0100

    Add tests for projects

--- tests/project.test.js
+++ tests/project.test.js
@@ -1,1 +1,30 @@
-project.test.js
+import createProject from '../src/models/project';
+import createTodo from '../src/models/todo';
+
+describe('Project', () => {
+  describe('createProject', () => {
+    test('Should create a project', () => {
+    let myProject = createProject('Study');
+    expect(myProject).toEqual({name: "Study", todoList: []});
+  });
+  });
+  describe('addTodo', () => {
+    test('Should add todo to a project', () => {
+      let myProject = createProject('Rails');
+      let myTodo = createTodo("Rspec", "Testing with Rspec", "24-01-2021", "High", 1);
+      myProject.addTodo(myTodo);
+      expect(myProject).toEqual({name: "Rails", todoList: [myTodo]});
+    });
+  });
+  describe('deleteTodo', () => {
+    test('Should delete todo from a project', () => {
+      let myProject = createProject('Rails');
+      let myTodo = createTodo("Rspec", "Testing with Rspec", "24-01-2021", "High", 1);
+      let anotherTodo = createTodo("Rspec", "Testing with Rspec", "24-01-2021", "High", 2)
+      myProject.addTodo(myTodo);
+      myProject.addTodo(anotherTodo);
+      myProject.deleteTodo(2);
+      expect(myProject).toEqual({name: "Rails", todoList: [myTodo]});
+    });
+  });
+});
